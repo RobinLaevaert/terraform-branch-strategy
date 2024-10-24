890a04e0acf9a4c2a1f3f306b9915196291632f5
Robin Laevaert <robin.laevaert@protonmail.com>
2024-10-24 15:32:32 +0200

    Datahub start

--- versions.tf
+++ versions.tf
@@ -8,6 +8,14 @@ terraform {
       source = "hashicorp/null"
       version = "3.2.3"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.16.1"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
   }
 
   backend "azurerm" {
